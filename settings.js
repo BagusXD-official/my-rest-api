9d6294072d6a367e10a57112e99526418ac2e17d
bagus321 <79736602+bagus321@users.noreply.github.com>
2024-02-06 16:46:30 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Bagong Ofc'// yourname
+global.MONGO_DB_URI = "mongodb+srv://tinibohay123:zD8BUzVzmE7qECUR@bagus.fjdbvrs.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "bagusart40@gmail.com" //email
+global.email_password = "baguscok" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
